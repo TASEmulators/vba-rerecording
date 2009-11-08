dff4d85d2e38304af79478018f1b413cc25ffd5e
gochaism <gochaism@bf3c5dd0-aae7-11dd-9a07-0f3041a8e97c>
2009-11-08 08:20:22 +0000

    apply recent lua changes of snes9x: * accept a table for color value. * add gui.parsecolor, joypad.getup, joypad.getdown, emu.emulating. * gui.line, gui.box, joypad.get becomes like Gens. * win32: add a menu to Lua window, add "Font..." and "Clear" items.

--- trunk/src/win32/resource.h
+++ trunk/src/win32/resource.h
@@ -613,6 +613,8 @@
 #define IDC_BUTTON_LUASTOP              1363
 #define IDC_LUACONSOLE                  1364
 #define IDC_BUTTON_LUAEDIT              1365
+#define IDC_LUACONSOLE_CLEAR            1366
+#define IDC_LUACONSOLE_CHOOSEFONT       1367
 #define ID_HELP_ABOUT                   40001
 #define ID_FILE_EXIT                    40002
 #define ID_OPTIONS_VIDEO_VSYNC          40009
@@ -1016,7 +1018,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        170
 #define _APS_NEXT_COMMAND_VALUE         42406
-#define _APS_NEXT_CONTROL_VALUE         1366
+#define _APS_NEXT_CONTROL_VALUE         1368
 #define _APS_NEXT_SYMED_VALUE           103
 #endif
 #endif
