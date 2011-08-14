5ce793e7fc5e27150d10cf26c9a6c18e458405cd
aquanull <aquanull@bf3c5dd0-aae7-11dd-9a07-0f3041a8e97c>
2011-08-14 10:28:17 +0000

    Renaming/moving...

--- branches/v24/win32/defaultconfig/SubWCRev.bat
+++
@@ -1,6 +0,0 @@
-if exist "userconfig\svnrev.h" goto :SkipCopy
-copy /Y "defaultconfig\svnrev.h" "userconfig\svnrev.h"
-
-:SkipCopy
-defaultconfig\SubWCRev.exe .. ".\defaultconfig\svnrev_template.h" ".\userconfig\svnrev.h"
-exit 0
