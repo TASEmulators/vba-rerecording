4af6a511f4132e1497479562a422a5556979a0d5
aquanull <aquanull@bf3c5dd0-aae7-11dd-9a07-0f3041a8e97c>
2011-08-23 05:35:01 +0000

    Transplanted r391 from v8.24 to v7.24.

--- branches/v7.24/src/gb/gbSound.cpp
+++ branches/v7.24/src/gb/gbSound.cpp
@@ -1110,6 +1110,7 @@ void gbSoundReadGame(int version, gzFile gzFile)
 	utilGzRead(gzFile, soundBuffer, 4 * 735);
 	utilGzRead(gzFile, soundFinalWave, 2 * 735);
 
+	// note: this resets the value of soundIndex to 0
 	if (version >= 7)
 	{
 		int quality = 1;
