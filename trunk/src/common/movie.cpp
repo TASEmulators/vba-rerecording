9a3de91935db21adfae7147da752743d5c93788f
aquanull <aquanull@bf3c5dd0-aae7-11dd-9a07-0f3041a8e97c>
2011-10-06 08:23:02 +0000

    Reapplying rev 394 that was mysteristically reverted on rev 299.

--- trunk/src/common/movie.cpp
+++ trunk/src/common/movie.cpp
@@ -327,10 +327,12 @@ static void remember_input_state()
 
 static void change_state(MovieState new_state)
 {
+#if 0
 	if (Movie.state == MOVIE_STATE_RECORD)
 	{
-		//truncate_movie(Movie.header.length_frames);
+		truncate_movie(Movie.header.length_frames);
 	}
+#endif
 
 #if (defined(WIN32) && !defined(SDL))
 	theApp.frameSearching	   = false;
@@ -1498,8 +1500,6 @@ int VBAMovieUnfreeze(const uint8 *buf, uint32 size)
 
 		Movie.currentFrame	 = current_frame;
 		Movie.inputBufferPtr = Movie.inputBuffer + Movie.bytesPerFrame * min(current_frame, Movie.header.length_frames);
-		
-		change_state(MOVIE_STATE_PLAY);
 	}
 	else
 	{
@@ -1521,14 +1521,14 @@ int VBAMovieUnfreeze(const uint8 *buf, uint32 size)
 		// for consistency, no auto movie conversion here since we don't auto convert the corresponding savestate
 		flush_movie_header();
 		flush_movie_frames();
-
-		change_state(MOVIE_STATE_RECORD);
 	}
+
+	change_state(MOVIE_STATE_PLAY);	// check for movie end
 
 	// necessary!
 	resetSignaled	  = false;
 	resetSignaledLast = false;
-	
+
 	// necessary to check if there's a reset signal at the previous frame
 	if (current_frame > 0)
 	{
@@ -1593,7 +1593,7 @@ bool VBAMovieSwitchToRecording()
 	change_state(MOVIE_STATE_RECORD);
 	systemScreenMessage("Movie re-record");
 
-	truncate_movie(Movie.currentFrame);
+	//truncate_movie(Movie.currentFrame);
 
 	return true;
 }
