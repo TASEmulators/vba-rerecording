137053b72cc4862bc65535443b8a35d7032ca811
aquanull <aquanull@bf3c5dd0-aae7-11dd-9a07-0f3041a8e97c>
2013-04-06 02:23:30 +0000

    No longer using png_structp internals directly.

--- trunk/src/win32/Dialogs/GBMapView.cpp
+++ trunk/src/win32/Dialogs/GBMapView.cpp
@@ -186,7 +186,7 @@ void GBMapView::savePNG(const char *name)
 		return;
 	}
 
-	if (setjmp(png_ptr->jmpbuf))
+	if (setjmp(png_jmpbuf(png_ptr)))
 	{
 		png_destroy_write_struct(&png_ptr, NULL);
 		fclose(fp);
--- trunk/src/win32/Dialogs/OamView.cpp
+++ trunk/src/win32/Dialogs/OamView.cpp
@@ -402,7 +402,7 @@ void OamView::savePNG(const char *name)
 		return;
 	}
 
-	if (setjmp(png_ptr->jmpbuf))
+	if (setjmp(png_jmpbuf(png_ptr)))
 	{
 		png_destroy_write_struct(&png_ptr, NULL);
 		fclose(fp);
